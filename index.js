cbd9c34c6f587f42c03acd12125ca3627b77795b
Owen Dresner <owendrez@gmail.com>
2022-01-25 16:56:44 -0500

    commit tree 9100c5f51db6b96f0c43ce3d12bbd4965d2a16b6

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(number1, number2){
+    return number1+number2
+}
+function subtract(number1, number2){
+    return number1-number2
+}
+function multiply(number1, number2){
+    return number1*number2
+}
+function divide(number1, number2){
+    return number1/number2
+}
+function increment(number){
+    number=number+1;
+    return number;
+}
+function decrement(number){
+    number=number-1;
+    return number;
+}
+function makeInt(number){
+    return parseInt(number, 10);
+}
+function preserveDecimal(number){
+    return parseFloat(number);
+}
